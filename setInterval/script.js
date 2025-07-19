e903cf7f48c001b9205baa79f081f58d10f238e5
Soumya Pradhan <pradhansoumya589@gmail.com>
2025-07-19 01:44:03 +0000

    setinterval

---
+++ setInterval/script.js
@@ -0,0 +1,11 @@
+let randomColor = function(){
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
+        
+    }
+    return color
+}
+
+console.log(randomColor());
