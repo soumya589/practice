c13fc55cb13454732bfe4db9a157bf4d61c1c744
Soumya Pradhan <pradhansoumya589@gmail.com>
2025-08-17 11:37:11 +0000

    foreach

--- reduce/reduce.js
+++ reduce/reduce.js
@@ -1,28 +1,51 @@
 
-let nums = [1,2,3,4]
-let fn = function(a , b){
-        return a + b;
+// let nums = [1,2,3,4]
+// let fn = function(a , b){
+//         return a + b;
         
+// }
+
+// let init = 100
+
+// const reduce  = function (nums, fn, init){
+
+//     let val = init;
+
+//     for (let i = 0; i < nums.length; i++){
+//         val = fn(val, nums[i])
+//     }
+
+//     console.log(val);
+    
+
+//     return val
+
+// }
+
+// reduce(nums, fn, init)
+
+
+let myArr = [1,2,3,4]
+let fn = function(a,b){
+    return a + b
 }
+let init = 0
 
-let init = 100
-
-const reduce  = function (nums, fn, init){
-
+function reduce(nums, fn, init){
     let val = init;
 
-    for (let i = 0; i < nums.length; i++){
-        val = fn(val, nums[i])
-    }
+    nums.forEach((num) => {
+        val = fn(val, num)
+        return val
+    });
 
     console.log(val);
     
 
     return val
-
 }
 
-reduce(nums, fn, init)
+reduce(myArr, fn, init)
 
 
 
